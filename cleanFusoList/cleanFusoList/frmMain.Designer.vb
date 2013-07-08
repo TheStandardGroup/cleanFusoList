3e73067688475e2552e33302bd982e3e84225edd
mbachmanTSG <mbachman@tsizone.com>
2013-07-08 12:39:57 -0400

    FIles for CleanFusoList

---
+++ cleanFusoList/cleanFusoList/frmMain.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.rbCanada = New System.Windows.Forms.RadioButton()
+        Me.rbUS = New System.Windows.Forms.RadioButton()
+        Me.tbPath = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.SuspendLayout
+        '
+        'rbCanada
+        '
+        Me.rbCanada.AutoSize = true
+        Me.rbCanada.Enabled = false
+        Me.rbCanada.Location = New System.Drawing.Point(100, 67)
+        Me.rbCanada.Name = "rbCanada"
+        Me.rbCanada.Size = New System.Drawing.Size(62, 17)
+        Me.rbCanada.TabIndex = 0
+        Me.rbCanada.TabStop = true
+        Me.rbCanada.Text = "Canada"
+        Me.rbCanada.UseVisualStyleBackColor = true
+        '
+        'rbUS
+        '
+        Me.rbUS.AutoSize = true
+        Me.rbUS.Enabled = false
+        Me.rbUS.Location = New System.Drawing.Point(100, 91)
+        Me.rbUS.Name = "rbUS"
+        Me.rbUS.Size = New System.Drawing.Size(89, 17)
+        Me.rbUS.TabIndex = 1
+        Me.rbUS.TabStop = true
+        Me.rbUS.Text = "United States"
+        Me.rbUS.UseVisualStyleBackColor = true
+        '
+        'tbPath
+        '
+        Me.tbPath.Enabled = false
+        Me.tbPath.Location = New System.Drawing.Point(13, 22)
+        Me.tbPath.Name = "tbPath"
+        Me.tbPath.Size = New System.Drawing.Size(161, 20)
+        Me.tbPath.TabIndex = 2
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(197, 19)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 3
+        Me.btnBrowse.Text = "Browse.."
+        Me.btnBrowse.UseVisualStyleBackColor = true
+        '
+        'btnRun
+        '
+        Me.btnRun.Enabled = false
+        Me.btnRun.Location = New System.Drawing.Point(45, 131)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 4
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = true
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(156, 131)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuit.TabIndex = 5
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = true
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 180)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.tbPath)
+        Me.Controls.Add(Me.rbUS)
+        Me.Controls.Add(Me.rbCanada)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Clean CSV Files"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents rbCanada As System.Windows.Forms.RadioButton
+    Friend WithEvents rbUS As System.Windows.Forms.RadioButton
+    Friend WithEvents tbPath As System.Windows.Forms.TextBox
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents btnRun As System.Windows.Forms.Button
+    Friend WithEvents btnQuit As System.Windows.Forms.Button
+
+End Class
---
+++ cleanFusoList/cleanFusoList/frmMain.vb
@@ -0,0 +1,122 @@
+﻿Public Class frmMain
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        Me.Dispose()
+    End Sub
+
+    'open the file for processing
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+        Dim strFileName As String
+
+        fd.Title = "Open File Dialog"
+        fd.InitialDirectory = "C:\"
+        fd.Filter = "All files (*.*)|*.*|CSV Files (*.csv)|*.csv"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+
+        'populate data fields
+        If fd.ShowDialog() = DialogResult.OK Then
+            strFileName = fd.FileName
+            tbPath.Text = (strFileName)
+            rbCanada.Enabled = True
+            rbCanada.Checked = True
+            rbUS.Enabled = True
+            btnRun.Enabled = True
+        End If
+    End Sub
+
+    'converts non quoted csv strings to quoted csv strings
+    Private Sub runMyStuff()
+        Dim path As String = tbPath.Text()
+        Dim isCanada As Boolean = False
+        Dim header As String
+        If rbCanada.Checked Then
+            isCanada = True
+            'header for canada
+            header = Chr(34) + "MDDLR" + Chr(34) + "," + Chr(34) + "DLR_NAME" + Chr(34) + "," + Chr(34) + "AOR_CITY" + Chr(34) + "," + Chr(34) + "AOR_STATE" + Chr(34) + "," + Chr(34) + "AOR_ZIP" + Chr(34) + "," + Chr(34) + "MDPOLKZIP" + Chr(34) + "," + Chr(34) + "RUNDATE" + Chr(34)
+        Else
+            'header for US
+            header = Chr(34) + "MDDLR" + Chr(34) + "," + Chr(34) + "DLR_NAME" + Chr(34) + "," + Chr(34) + "AOR_CITY" + Chr(34) + "," + Chr(34) + "AOR_COUNTY" + Chr(34) + "," + Chr(34) + "AOR_STATE" + Chr(34) + "," + Chr(34) + "MDPOLKZIP" + Chr(34) + "," + Chr(34) + "MDCNRY" + Chr(34) + "," + Chr(34) + "MDCURN" + Chr(34) + "," + Chr(34) + "MDREGN" + Chr(34) + "," + Chr(34) + "MDDIST" + Chr(34) + "," + Chr(34) + "RUNDATE" + Chr(34)
+        End If
+        Dim myReader = New Microsoft.VisualBasic.FileIO.TextFieldParser(path)
+        myReader.SetDelimiters(",")
+        Dim currentRow As String()
+        Dim fileList As ArrayList = New ArrayList
+        fileList.Add(header)
+        Dim count = 0
+        'read the file
+        While Not myReader.EndOfData
+            Dim myStr As String = ""
+            Try
+                currentRow = myReader.ReadFields()
+                'only add non header info
+                If count > 0 Then
+                    Dim count2 = 0
+                    For Each field In currentRow
+                        If isCanada Then
+                            If count2 < 6 Then
+                                If field = "UNASS" Then
+                                    field = "OPEN"
+                                End If
+                                myStr += "" + Chr(34) + field + Chr(34) + ","
+                            End If
+                            If count2 = 5 Then
+
+                                Dim todaysdate As String = String.Format("{0:MM/dd/yyyy}", DateTime.Now)
+                                myStr += Chr(34) + todaysdate + Chr(34)
+                            End If
+                        Else
+                            myStr += "" + Chr(34) + field + Chr(34)
+                            If count2 < 10 Then
+                                myStr += ","
+                            End If
+                        End If
+                        count2 += 1
+
+                    Next
+                    fileList.Add(myStr)
+                End If
+            Catch ex As Microsoft.VisualBasic.
+                                FileIO.MalformedLineException
+                MsgBox("Line " & ex.Message &
+                        "is not valid and will be skipped.")
+            Finally
+                count += 1
+            End Try
+        End While
+        Dim worked = False
+       
+        buildCleanCSV(fileList)
+    End Sub
+
+    Private Sub buildCleanCSV(ByRef vals As ArrayList)
+
+        Dim pat = ""
+        Dim myList As String() = tbPath.Text.Split("\")
+        Dim myFileName As String() = myList(myList.Count - 1).Split(".")
+        myList(myList.Count - 1) = myFileName(0) & "_clean" & ".csv"
+        Dim count = 0
+        For Each item In myList
+            pat += item.ToString()
+            If count < myList.Count - 1 Then
+                pat += "\"
+            End If
+            count += 1
+        Next
+        MessageBox.Show(pat)
+        System.IO.File.Create(pat).Dispose()
+        count = 0
+        Dim objWriter As New System.IO.StreamWriter(pat, True)
+        While count < vals.Count
+            objWriter.WriteLine(vals(count))
+            count += 1
+        End While
+        objWriter.Close()
+        MessageBox.Show("Finished Processing the CSV file", "Alert")
+    End Sub
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        runMyStuff()
+    End Sub
+End Class
